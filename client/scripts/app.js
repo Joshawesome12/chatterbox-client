08572c99cbfec90918e4e4e02d1be741991cf8dd
joshawesome12 <joshawesome12@yahoo.com>
2017-10-13 19:14:00 -0500

    Progress

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,62 +1,52 @@
 // YOUR CODE HERE:
 var app = {
-
 };
-
-app.init = function () {
-  return true;
+app.init = function() {
+  app.$main = $('#main');
+  app.$chats = $('#chats');
+  app.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
+  
+  $('#msgForm').on('submit', function(event) {
+    event.preventDefault();
+    console.log(event.name);
+    app.send();
+    app.fetch();
+  });
+  app.fetch();
 };
-
-app.send = function(message){
-	$.ajax({
-  url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: message,
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
-}
-
-
-app.fetch = function(){
-	$.ajax({
-  url: undefined,
-  type: 'GET',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message recieved');
-  },
-  error: function (data) {
-    console.error('chatterbox: Failed to retrieve message', data);
-  }
-});
-}
-
-// ### API Keys:
-//  * Server: http://parse.atx.hackreactor.com/chatterbox/classes/messages
-//  * App ID: 28f10c64a8b1b900a057b74cabaebaf474573436
-//  * API Key: 7543f778eede3b2723e3018977563e69738d7c1b
-
-app.clearMessages = function(){
-	return $.ajax({
-    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
-    type: 'DELETE',
-    // success: callback,
+app.send = function(message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
     data: message,
     contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
-}
-
-app.renderMessage = function(message){
-
-}
-
-app.renderRoom = function(){
-
-}
+};
+app.fetch = function(message) {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message retrieved');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to fetch message', data);
+    }
+  });
+};
+app.clearMessages = function() {
+  app.$chats.children().remove();
+};
+app.renderMessage = function(message) {
+};
+app.renderRoom = function(room) {
+    
+};
